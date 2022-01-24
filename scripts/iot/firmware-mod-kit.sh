06a5cce52b66a2fbf7e44d233c84385aa32afce3
g0pher98 <mail@jseung.me>
2022-01-24 15:42:59 +0900

    add angr, modify fmk

--- scripts/iot/firmware-mod-kit.sh
+++ scripts/iot/firmware-mod-kit.sh
@@ -1,6 +1,7 @@
 sudo apt -y install firmware-mod-kit
 
 ln -s /opt/firmware-mod-kit/trunk/extract-firmware.sh /usr/bin/extract-firmware
+ln -s /opt/firmware-mod-kit/trunk/extract-multisquashfs-firmware.sh /usr/bin/extract-multisquashfs-firmware
 
 ln -s /opt/firmware-mod-kit/trunk/uncpio.sh /usr/bin/uncpio
 ln -s /opt/firmware-mod-kit/trunk/uncramfs_all.sh /usr/bin/uncramfs
